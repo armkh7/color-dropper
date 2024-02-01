f08b8c4942f442e12fee812f7469e231d3b2e8ef
armkh7 <armkhachatryan3@gmail.com>
2024-02-01 19:51:54 +0400

    main functionalities added for canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // components
 import Header from "./components/Header";
+import ColorDropper from "./components/ColorDropper";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <ColorDropper />
     </main>
   );
 }
